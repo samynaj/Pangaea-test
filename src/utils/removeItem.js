df52a93543499a8e861df0d9d90d68bef563e31d
samynaj <samuelnnj@gmail.com>
2021-07-30 15:18:46 +0100

    Update: utility function

---
+++ src/utils/removeItem.js
@@ -0,0 +1,16 @@
+
+const removeItemsFromCart = (cartItems, cartItemToRemove) => {
+
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(cartItem => 
+        cartItem.id === cartItemToRemove.id ? {...cartItem, quantity: cartItem.quantity - 1} : cartItem
+    )
+    
+};
+
+export default removeItemsFromCart
