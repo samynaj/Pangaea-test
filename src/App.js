1315ad172253280b0d8be6ca4a536e2c0cd3f14d
samynaj <samuelnnj@gmail.com>
2021-07-30 15:14:55 +0100

    Update: apollo setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
-import Shop from './pages/Shop'
+import Shop from './pages/Shop';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+
+const errorLink = onError(({graphqlErrors, networkError}) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) => {
+      return alert(`Graphql error ${message}`)
+    })
+  }
+})
+const link = from([ 
+  errorLink,
+  new HttpLink({uri: 'https://pangaea-interviews.now.sh/api/graphql'})
+])
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+})
 
 function App() {
   return (
-    <div className="app">
-      <Shop />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="app">
+        <Shop />
+      </div>
+    </ApolloProvider>
   );
 }
 
