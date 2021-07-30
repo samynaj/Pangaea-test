9c669ae105a6259111740bb5ec16f810f99834b8
samynaj <samuelnnj@gmail.com>
2021-07-30 15:15:33 +0100

    Update: worked on the header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core'
-import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined'
+import MenuIcon from '@material-ui/icons/Menu'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
-function Index() {
+function Index({CartItemNum, openCart}) {
+    const [menu, setMenu] = useState(false);
 
     const useStyles = makeStyles(() => ({
         root: {
@@ -15,11 +18,17 @@ function Index() {
             alignItems: 'center',
             boxShadow: '0px 2px 3px -3px rgb(128, 128, 128)',
             lineHeight: 3,
+            '@media (max-width: 768px)': {
+                padding: '20px'
+            }
         },
         leftSide: {
             width: '30%',
             display: 'flex',
             justifyContent: 'space-between',
+            '@media (max-width: 768px)': {
+                display: 'none'
+            }
         },
         logo: {
             width: 'auto',
@@ -42,8 +51,13 @@ function Index() {
             width: '10%',
             display: 'flex',
             justifyContent: 'space-between',
-            alignItems: 'center'
-            // background: 'yellow'
+            alignItems: 'center',
+            '@media (max-width: 768px)': {
+                width: '20%'
+            },
+            '@media (max-width: 425px)': {
+                width: '25%'
+            }
         },
         cart: {
             marginTop: '17px',
@@ -55,6 +69,76 @@ function Index() {
             right: '-8px',
             fontWeight: 600,
             fontSize: '1rem'
+        },
+        menu: {
+            display: 'none',
+            '@media (max-width: 768px)': {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }
+        },
+        sidebar: {
+            display: 'none',
+            '@media (max-width: 768px)': {
+                width: '500px',
+                height: '100%',
+                position: 'fixed',
+                marginLeft: '-100%',
+                transition: '850ms',
+                display: 'flex',
+            },
+            '@media (max-width: 425px)': {
+                width: '100%'
+            },
+        },
+        slide: {
+            display: 'none',
+            '@media (max-width: 768px)': {
+                width: '600px',
+                height: '100%',
+                position: 'fixed',
+                left: '0',
+                top: '0',
+                transition: '850ms',
+                display: 'flex',
+                flexDirection: 'column',
+                zIndex: '7',
+                background: '#fff',
+                padding: '40px'
+            },
+            '@media (max-width: 425px)': {
+                width: '100%'
+            },
+        },
+        menuTop: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+        },
+        menuTopNav: {
+            width: '30%',
+            display: 'flex',
+            justifyContent: 'space-between',
+        },
+        menuTopNavItem: {
+            fontWeight: 600,
+        },
+        menuTopNavBack: {
+            width: '20px',
+            height: '20px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '50%',
+            border: '1px solid #d8dbd7'
+        },
+        menuList: {
+            marginTop: '30px'
+        },
+        menuItem: {
+            fontSize: '20px',
+            fontWeight: 800,
         }
     }));
 
@@ -68,10 +152,31 @@ function Index() {
                     <p className={classes.navItem}>Learn</p>
                 </nav>
             </div>
+            <div className={menu? classes.slide : classes.sidebar}>
+                <div className={classes.menuTop}>
+                    <nav className={classes.menuTopNav}>
+                        <p className={classes.menuTopNavItem}>Shop</p>
+                        <p className={classes.menuTopNavItem}>Learn</p>
+                    </nav>
+                    <div className={classes.menuTopNavBack} onClick={() => setMenu(false)}>
+                        <ArrowBackIosIcon style={{fontSize: '15px'}}/>
+                    </div>
+                </div>
+                <div className={classes.menuList}>
+                    <p className={classes.menuItem}>Skin</p>
+                    <p className={classes.menuItem}>Hair & Body</p>
+                    <p className={classes.menuItem}>Sets</p>
+                    <p className={classes.menuItem}>Accessories</p>
+                    <p className={classes.menuItem}>Shop All</p>
+                </div>
+            </div>
+            <div className={classes.menu} onClick={() => setMenu(true)} >
+                <MenuIcon fontSize='medium'/>
+            </div>
             <div className={classes.rightSide}>
                 <p className={classes.navItem}>Account</p>
-                <div className={classes.cart}>
-                    <p className={classes.cartNum}>0</p>
+                <div className={classes.cart} onClick={openCart}>
+                    <p className={classes.cartNum}>{CartItemNum}</p>
                     <ShoppingCartOutlinedIcon style={{fontSize: 25}}/>
                 </div>
             </div>
